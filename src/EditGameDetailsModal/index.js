f70712e96df1f79c3e0ec5a66ef60c74f7cfe882
matthew Robinson <matthewrobinson@matthews-MacBook-Air-3.local>
2019-06-06 17:11:57 -0500

    90 percent with the edit event details modal

--- src/EditGameDetailsModal/index.js
+++ src/EditGameDetailsModal/index.js
@@ -1,22 +1,65 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 
-const EditGameDetailsModal = (props) => {
+
 	
-	console.log(props);
+class EditGameDetailsModal extends Component {
+	constructor(props){
+		super(props);
 
-	return(
+		this.state = {
+			viewGame: this.props.viewGame
+		}
+	}
 
-		<Fragment>
+	componentDidMount(){
+		this.setState({
+			viewGame:this.props.viewGame
+		})
+	}
+
+	render(){
+
+		console.log(this.props.viewGame,'<-- this is props in the edit game details modal ');
+		console.log(this.state.viewGame, '<--- this is STATE in the edit game details modal');
+
+		return(
+			<Fragment>
 			This is the edit game details modal
 			<div>
-				Date: 
-				Description:
-				Location:
-				Name:
-				Sport:
+				Date:<input 
+					text='date'
+					name='date'
+					value={this.state.viewGame.date}
+					onChange={this.handleChange}
+				/>
+				Description:<input 
+					text='text'
+					name='description'
+					value={this.state.viewGame.description}
+					onChange={this.handleChange}
+				/>
+				Location:<input 
+					text='text'
+					name='location'
+					value={this.state.viewGame.location}
+					onChange={this.handleChange}
+				/>
+				Name:<input 
+					text='text'
+					name='name'
+					value={this.state.viewGame.name}
+					onChange={this.handleChange}
+				/>
+				Sport:<input 
+					text='text'
+					name='sport'
+					value={this.state.viewGame.sport}
+					onChange={this.handleChange}
+				/>
 			</div>
 		</Fragment>
-	)
+		)
+	}
 }
 
 
